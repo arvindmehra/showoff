069c6fcac119548fc6e1742165cd67752f7ce541
Arvind Mehra <arvind_mehra@external.mckinsey.com>
2020-03-06 19:03:05 +0530

    If user signed in Show user’s all widgets

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -4,11 +4,13 @@ class WidgetsController < ApplicationController
   def index
     response = RestClient.get("#{API_URL}/widgets/visible",
     {params:  {client_id: CLIENT_ID, client_secret: CLIENT_SECRET}, term: 'a'})
-
-
     widgets_json = JSON.parse response.body
-
-
     @widgets = widgets_json["data"]["widgets"]
+    if user_signed_in?
+      puts current_user_token
+      response = RestClient.get("#{API_URL}/widgets/", {:Authorization => "Bearer #{current_user_token}"})
+      widgets_json = JSON.parse response.body
+      @user_widgets = widgets_json["data"]["widgets"]
+    end
   end
 end
