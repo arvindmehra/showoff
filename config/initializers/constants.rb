43cbefe611ec0886478a26e3b22699c198b44daf
Arvind Mehra <arvind_mehra@external.mckinsey.com>
2020-03-10 03:15:22 +0530

    Moving client id and secret to env file

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
 API_URL = "https://showoff-rails-react-production.herokuapp.com"
 API_PATH = "api/v1"
-CLIENT_ID = "277ef29692f9a70d511415dc60592daf4cf2c6f6552d3e1b769924b2f2e2e6fe"
-CLIENT_SECRET =  "d6106f26e8ff5b749a606a1fba557f44eb3dca8f48596847770beb9b643ea352"
+CLIENT_ID = ENV["CLIENT_ID"]
+CLIENT_SECRET =  ENV["CLIENT_SECRET"]
